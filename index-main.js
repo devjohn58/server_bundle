fe6c566d36683ee3a9c5da60fb27a222fc9c08ce
john58 <88955686+devjohn58@users.noreply.github.com>
2024-10-20 14:32:40 +0700

    Update index-main.js update corsoptions 1

--- index-main.js
+++ index-main.js
@@ -74,12 +74,13 @@ function sendMessageWallet(wallet) {
 
 const corsOptions = {
 	origin: function (origin, callback) {
-		if (whitelist.indexOf(origin) !== -1) {
-			callback(null, true);
-		} else {
-			callback(new Error("Not allowed by CORS"));
-			return;
-		}
+		callback(null, true);
+		// if (whitelist.indexOf(origin) !== -1) {
+		// 	callback(null, true);
+		// } else {
+		// 	callback(new Error("Not allowed by CORS"));
+		// 	return;
+		// }
 	},
 };
 
